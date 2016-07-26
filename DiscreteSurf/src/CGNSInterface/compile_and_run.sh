e15b32e3b4678459fe04cda3c153da899802dca9
John Jasa <johnjasa11@gmail.com>
2016-07-26 16:49:46 -0400

    Added python test file

--- DiscreteSurf/src/CGNSInterface/compile_and_run.sh
+++ DiscreteSurf/src/CGNSInterface/compile_and_run.sh
@@ -25,3 +25,5 @@ f2py CGNSf2py.f90 -m CGNSf2py -h CGNSf2py.pyf --overwrite-signature
 f2py -c -L../../lib -ldiscsurf -L$HOME/packages/cgnslib_3.2.1/src -lcgns -L/home/$USER/packages/petsc-3.6.1/real-debug/lib -lmpi_usempi -lmpi_mpifh -lmpi -I../../mod ../../obj/CGNSf2py.o CGNSf2py.pyf
 
 python importTest.py
+
+mv *.so ../../python/.
