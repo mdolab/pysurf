2de1c68f66c90b9f83bc4b8ccb95d59545ade239
Ney Secco <neysecco@umich.edu>
2016-08-08 12:25:53 -0400

    Added code to merge nearby nodes when generating intersection FE data.

--- TSurf/src/intersections/Intersection.F90
+++ TSurf/src/intersections/Intersection.F90
@@ -300,6 +300,150 @@ end subroutine filterElements
 
 !============================================================
 
+subroutine condenseBarFEs(distTol, coor, barsConn, newCoor)
+
+  ! This subroutine receives a list of bar FE which may have repeated points and
+  ! Then condenses (merges) points that are closer than a given tolerance.
+  ! This helps us getting a continuous line from the intersection algorithm results.
+  ! Ney Secco 2016-08
+  !
+  ! INPUTS
+  !
+  ! distTol: real -> Distance tolerance used to determine if two nodes are effectively the same.
+  !
+  ! coor: real(3,nNodes) -> Nodal coordinates. May have repeated coordinates.
+  !
+  ! OUTPUTS
+  !
+  ! newCoor: real(3,nUniqueNodes) -> Nodal coordinates with unique nodes only.
+  !
+  ! INPUTS/OUTPUTS
+  !
+  ! barsConn: int(2,nElements) -> Bar element connectivity array. Its values will be updated
+  !           to use newCoor indices.
+
+
+  implicit none
+
+  ! INPUTS
+  real(kind=realType), intent(in) :: distTol
+  real(kind=realType), dimension(:,:), intent(in) :: coor
+
+  ! OUTPUTS
+  real(kind=realType), dimension(:,:), allocatable, intent(out) :: newCoor
+
+  ! INPUTS/OUTPUTS
+  integer(kind=intType), dimension(:,:), intent(inout) :: barsConn
+
+  ! WORKING
+  integer(kind=intType) :: nNodes, nElem, nUniqueNodes, nCopies
+  integer(kind=intType) :: currNodeID, prevNodeID, link, elemID
+  real(kind=realType), dimension(3) :: currCoor, prevCoor
+  real(kind=realType) :: dist
+  integer(kind=intType), dimension(size(coor,2)) :: linkOld2New
+
+  ! EXECUTION
+
+  ! Get problem size
+  nNodes = size(coor,2)
+  nElem = size(barsConn,2)
+
+  ! Initialize number of unique nodes found so far.
+  ! The first node is a unique one =P!
+  nUniqueNodes = 1
+
+  ! As the first node is unique, its old-to-new link should point to itself
+  linkOld2New(1) = 1
+
+  ! Loop over the nodes to find the unique ones
+  do currNodeID = 2,nNodes
+
+     ! Get coordinates of current node
+     currCoor = coor(:,currNodeID)
+
+     ! Now loop over the previous nodes to find if it is repeated
+     do prevNodeID = 1,currNodeID-1
+
+        ! Get coordinates of the previous node
+        prevCoor = coor(:,prevNodeID)
+
+        ! Compute distance between nodes
+        dist = norm2(currCoor - prevCoor)
+
+        ! Check if the distance is below the merging tolerance
+        if (dist .le. distTol) then
+
+           ! Update link array.
+           ! The array linkOld2New will contain newCoor indices that correspond to each
+           ! coor index.
+           ! So the current node should use the same link as the previous node, as they will
+           ! point to the same index of the new coordinate array.
+           linkOld2New(currNodeID) = linkOld2New(prevNodeID)
+
+           ! We can jump out of the prevNode do loop and check the next node.
+           exit
+
+        end if
+
+     end do
+
+     ! Check if we did not find any copy. In this case, we need to initialize a new
+     ! unique node
+     if (prevNodeID .eq. currNodeID) then
+
+        ! Increase the number of unique nodes found so far
+        nUniqueNodes = nUniqueNodes + 1
+
+        ! Create new link
+        linkOld2New(currNodeID) = nUniqueNodes
+
+     end if
+
+  end do
+
+  ! Now that we know the number of unique nodes, we can allocate memory for the new
+  ! coordinate array.
+  allocate(newCoor(3,nUniqueNodes))
+
+  ! Initialize number of nodes copied so far
+  nCopies = 0
+
+  ! We loop once again over the nodes so we can copy the unique values
+  do currNodeID = 1,nNodes
+
+     ! Get coordinates of current node
+     currCoor = coor(:,currNodeID)
+
+     ! Get index of the current node in the new coordinate array
+     link = linkOld2New(currNodeID)
+     
+     ! Check if the new link is already used
+     if (link .gt. nCopies) then
+
+        ! Increment number of copies done so far
+        nCopies = nCopies + 1
+
+        ! Copy coordinates
+        newCoor(:,nCopies) = currCoor
+
+     end if
+        
+  end do
+
+  ! Now the last step is updating the bars connectivity.
+  ! Loop over the elements
+  do elemID = 1,nElem
+
+     ! Update connectivities
+     barsConn(1,elemID) = linkOld2New(barsConn(1,elemID))
+     barsConn(2,elemID) = linkOld2New(barsConn(2,elemID))
+
+  end do
+
+end subroutine condenseBarFEs
+
+!============================================================
+
 subroutine triTriIntersect(V0, V1, V2, U0, U1, U2, intersect)
 
   real(kind=realType), dimension(3), intent(in) :: V0, V1, V2, U0, U1, U2
