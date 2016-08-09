0e3afffb75084910b241a71790c6bd2d8b940d7e
John Jasa <johnjasa11@gmail.com>
2016-08-09 11:58:53 -0400

    Fixed bug concerning ADTapi typing

--- TSurf/python/intTest.py
+++ TSurf/python/intTest.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 tt = intersectionapi.testtri
 
-def find_int_and_plot(tri1, tri2):
+def find_int_and_plot(tri1, tri2, ax):
 
     u0 = tri1[0, :]
     u1 = tri1[1, :]
@@ -17,10 +17,6 @@ def find_int_and_plot(tri1, tri2):
 
     b, s, e = tt(u0, u1, u2, v0, v1, v2)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_aspect('equal')
-
     ax.plot([u0[0], u1[0]], [u0[1], u1[1]], [u0[2], u1[2]], color='b')
     ax.plot([u1[0], u2[0]], [u1[1], u2[1]], [u1[2], u2[2]], color='b')
     ax.plot([u2[0], u0[0]], [u2[1], u0[1]], [u2[2], u0[2]], color='b')
@@ -31,73 +27,23 @@ def find_int_and_plot(tri1, tri2):
 
     ax.plot([s[0], e[0]], [s[1], e[1]], [s[2], e[2]], color='r')
 
+    return ax
+
+
+###################################
+
+for j in range(5):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_aspect('equal')
+
+    tri_list = []
+
+    for i in range(4):
+        tri_list.append(np.random.rand(3, 3))
+
+    for tri1 in tri_list:
+        for tri2 in tri_list:
+            ax = find_int_and_plot(tri1, tri2, ax)
+
     plt.show()
-
-###################################
-# Orthogonal and share edge
-tri1 = np.array([[-1., -1., -1.],
-                 [1., 1., 1.],
-                 [-.5, -.5, 1.]])
-
-tri2 = np.array([[-.5, -.5, -.5],
-                 [.5, .5, .5],
-                 [1., 0., 0.]])
-
-# find_int_and_plot(tri1, tri2)
-
-###################################
-# Orthogonal and intersect
-tri1 = np.array([[-2., 0., 0.],
-                 [2., 0., 0.],
-                 [0., 2., 0.]])
-
-tri2 = np.array([[-2., 1., -1.],
-                 [2., 1., -1.],
-                 [0., 1., 2.]])
-
-# find_int_and_plot(tri1, tri2)
-
-###################################
-# Askew and intersect
-tri1 = np.array([[-2., 0., 1.],
-                 [2., 0., 0.],
-                 [0., 2., 2.]])
-
-tri2 = np.array([[-2., 1., -1.],
-                 [2., 1., -1.],
-                 [0., 1., 2.]])
-
-# find_int_and_plot(tri1, tri2)
-
-###################################
-# Coplanar and fully enclosed
-tri1 = np.array([[-2., 2., 0.],
-                 [2., 2., 0.],
-                 [0., -2., 0.]])
-
-tri2 = np.array([[-1., 1., 0.],
-                 [1., 1., 0.],
-                 [0., -1., 0.]])
-
-# find_int_and_plot(tri1, tri2)
-
-###################################
-# Coplanar and share a side
-tri1 = np.array([[-1., 1., 0.],
-                 [1., 1., 0.],
-                 [0., -1., 0.]])
-
-tri2 = np.array([[-1., 0., 0.],
-                 [1., 0., 0.],
-                 [0., 1., 0.]])
-
-# find_int_and_plot(tri1, tri2)
-
-
-###################################
-for i in range(100):
-    tri1 = np.random.rand(3, 3)
-
-    tri2 = np.random.rand(3, 3)
-
-    find_int_and_plot(tri1, tri2)
