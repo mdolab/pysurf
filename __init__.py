af20fdc10aba49f4372a09bc0cce9672fae587fb
John Jasa <johnjasa11@gmail.com>
2016-08-09 14:26:59 -0400

    Added tecplot output for curves

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from baseClasses import Component
 from TSurf.python.tsurf_component import TSurfComponent
 from TSurf.python.tsurf_intersection import compute_intersections
 from GeoMACHSurf.geomach_component import GeoMACHComponent
+import plot3d_interface
