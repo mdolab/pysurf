Fixed intersection derivatives.

examples/test/tsurf_intersection_test_deriv.py
@@ -18,6 +18,8 @@ body = pysurf.TSurfGeometry('../inputs/crm.cgns',['b_fwd','b_cnt','b_rrf'])
 comp1 = wing
 comp2 = body
 
+distTol = 1e-7
+
 #========================================================
 
 def test_curve_intersection(deltaZ,ii):
@@ -38,15 +40,6 @@ def test_curve_intersection(deltaZ,ii):
         if 'int' in curve:
             intCurve = comp1.curves[curve]
 
-            # Running backward mode
-
-            coorIntb = np.random.rand(intCurve.coor.shape[0],intCurve.coor.shape[1])
-
-            coorAb, coorBb = pysurf.tsurf_tools._compute_pair_intersection_b(comp1,
-                                                                             comp2,
-                                                                             intCurve,
-                                                                             coorIntb)
-
             # Running forward mode
 
             coorAd = np.array(np.random.rand(comp1.coor.shape[0],comp1.coor.shape[1]),order='F')
@@ -56,20 +49,39 @@ def test_curve_intersection(deltaZ,ii):
                                                                        comp2,
                                                                        intCurve,
                                                                        coorAd,
-                                                                       coorBd)
+                                                                       coorBd,
+                                                                       distTol)
+
+            newCoorIntd = pysurf.tsurf_tools._remesh_d(intCurve, coorIntd)
+
+
+            # Running backward mode
+
+            newCoorIntb = np.random.rand(intCurve.coor.shape[0],intCurve.coor.shape[1])
+
+            coorIntb = pysurf.tsurf_tools._remesh_b(intCurve, newCoorIntb)
+
+            coorAb, coorBb = pysurf.tsurf_tools._compute_pair_intersection_b(comp1,
+                                                                             comp2,
+                                                                             intCurve,
+                                                                             coorIntb,
+                                                                             distTol)
 
             # Dot product test
             dotProd = 0.0
-            dotProd = dotProd + np.sum(coorIntb*coorIntd)
+            dotProd = dotProd + np.sum(newCoorIntb*newCoorIntd)
             dotProd = dotProd - np.sum(coorAb*coorAd)
             dotProd = dotProd - np.sum(coorBb*coorBd)
 
-            # Print results
+            # Print results; should be 0
             print 'dotProd test'
             print dotProd
 
+            # Remesh the curve in a linear spacing
+            newIntCurve = intCurve.remesh()
+
             # Save the curve
-            intCurve.export_plot3d('curve_%03d'%ii)
+            newIntCurve.export_plot3d('curve_%03d'%ii)
 
             # Find the upper skin trailing edge point
             pt0 = intCurve.barsConn[0,0]
@@ -81,17 +93,34 @@ def test_curve_intersection(deltaZ,ii):
             else:
                 pointID = pt1
 
-            Y = intCurve.coor[1,pointID-1]
-            coorIntb[:,:] = 0.0
-            coorIntb[1,pointID-1] = 1.0
+            # Now compute the derivative for the Y coordinate of the first point of the intersection
+            X = newIntCurve.coor[0,pointID-1]
+            Y = newIntCurve.coor[1,pointID-1]
+            Z = newIntCurve.coor[2,pointID-1]
+            newCoorIntb[:,:] = 0.0
+            newCoorIntb[1,pointID-1] = 1.0
+
+            # Get nodes of the parent triangles
+            parentTriaA = newIntCurve
+
+            # Compute the remesh derivatives
+            coorIntb = pysurf.tsurf_tools._remesh_b(intCurve, newCoorIntb)
+
             coorAb, coorBb = pysurf.tsurf_tools._compute_pair_intersection_b(comp1,
                                                                              comp2,
                                                                              intCurve,
-                                                                             coorIntb)
+                                                                             coorIntb,
+                                                                             distTol)
             dYdZ = np.sum(coorAb[2,:])
 
     # Remove translation
-    comp1.translate(0,+100.0,-deltaZ)
+    comp1.translate(0.0,+100.0,-deltaZ)
+
+    # Print results
+    print 'results'
+    print 'Y:',Y
+    print 'dYdZ:',dYdZ
+    print ''
 
     # Return results
     return Y, dYdZ
@@ -99,7 +128,7 @@ def test_curve_intersection(deltaZ,ii):
 #========================================================
 
 # MIN PROGRAM
-nStates = 20
+nStates = 15
 Z = np.linspace(0.0, 140.0, nStates)
 
 Y = np.zeros(len(Z))

geometryEngines/TSurf/python/tsurf_tools.py
@@ -813,8 +813,8 @@ def _compute_pair_intersection(TSurfGeometryA, TSurfGeometryB, distTol):
 
     if len(coor) > 0:
 
-        barsConn = np.sort(barsConn, axis=0)
-        barsConn = np.vstack({tuple(col) for col in barsConn.T}).T
+        #barsConn = np.sort(barsConn, axis=0)
+        #barsConn = np.vstack({tuple(col) for col in barsConn.T}).T
 
         # Sort FE data. After this step, barsConn may become a list if we
         # have two disconnect intersection curves.
@@ -859,7 +859,7 @@ def _compute_pair_intersection(TSurfGeometryA, TSurfGeometryB, distTol):
 
 #=================================================================
 
-def _compute_pair_intersection_b(TSurfGeometryA, TSurfGeometryB, intCurve, coorIntb):
+def _compute_pair_intersection_b(TSurfGeometryA, TSurfGeometryB, intCurve, coorIntb, distTol):
 
     '''
     This function is the backward mode of the intersection computation.
@@ -894,14 +894,15 @@ def _compute_pair_intersection_b(TSurfGeometryA, TSurfGeometryB, intCurve, coorI
                                                                            intCurve.coor,
                                                                            coorIntb,
                                                                            intCurve.barsConn,
-                                                                           intCurve.extra_data['parentTria'])
+                                                                           intCurve.extra_data['parentTria'],
+                                                                           distTol)
 
     # Return derivatives
     return coorAb, coorBb
 
 #=================================================================
 
-def _compute_pair_intersection_d(TSurfGeometryA, TSurfGeometryB, intCurve, coorAd, coorBd):
+def _compute_pair_intersection_d(TSurfGeometryA, TSurfGeometryB, intCurve, coorAd, coorBd, distTol):
 
     '''
     This function is the backward mode of the intersection computation.
@@ -918,6 +919,9 @@ def _compute_pair_intersection_d(TSurfGeometryA, TSurfGeometryB, intCurve, coorA
     coorAd: float(3,nNodesA) -> Derivative seeds of the component A nodal coordinates
 
     coorBd: float(3,nNodesB) -> Derivative seeds of the component B nodal coordinates
+
+    distTol: float -> Distance used to check if the bar elements were flipped. Used the
+                      same distTol used to merge nearby nodes.
 
     OUTPUTS:
 
@@ -937,7 +941,8 @@ def _compute_pair_intersection_d(TSurfGeometryA, TSurfGeometryB, intCurve, coorA
                                                                      TSurfGeometryB.quadsConn,
                                                                      intCurve.coor,
                                                                      intCurve.barsConn,
-                                                                     intCurve.extra_data['parentTria'])
+                                                                     intCurve.extra_data['parentTria'],
+                                                                     distTol)
 
     # Return derivatives
     return coorIntd