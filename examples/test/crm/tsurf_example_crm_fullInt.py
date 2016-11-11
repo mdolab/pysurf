Added curve tangent derivatives. Fixed remesh function for periodic curves. Updated intersection function to output dicionary of curves. Added forward derivatives for projections in hypsurf.

examples/test/crm/tsurf_example_crm_fullInt.py
@@ -13,13 +13,13 @@ import copy
 # USER INPUTS
 
 # Define translation cases for the wing
-nStates = 1
+nStates = 11
 
 wingTranslation = np.zeros((nStates,3))
 wingTranslation[:,1] = np.linspace(-10.0, -100.0, nStates)
 wingTranslation[:,2] = np.linspace(0.0, 140.0, nStates)
 
-#wingTranslation = [wingTranslation[0,:]]
+wingTranslation = [wingTranslation[0,:]]
 
 wingRotation = [0.0 for s in range(len(wingTranslation))]
 
@@ -27,12 +27,11 @@ wingRotation = [0.0 for s in range(len(wingTranslation))]
 fps = 2
 
 # Load components
-wing = pysurf.TSurfGeometry('../../inputs/crm.cgns',['w_upp','w_low','te_low_curve','te_upp_curve','w_le_curve'])
+wing = pysurf.TSurfGeometry('../../inputs/crm.cgns',['w_upp','w_low','w_ted','te_low_curve','te_upp_curve'])
 body = pysurf.TSurfGeometry('../../inputs/crm.cgns',['b_fwd','b_cnt','b_rrf'])
 
 # Flip some curves
 wing.curves['te_upp_curve'].flip()
-
 
 # Define function to generate a wing body mesh for a given wing position and angle
 
@@ -40,7 +39,7 @@ def generateWingBodyMesh(wingTranslation, wingRotation, meshIndex):
 
     # OPTIONS
     generate_wing = True
-    generate_body = True
+    generate_body = False
 
     # DEFINE FUNCTION TO GENERATE MESH
 
@@ -66,10 +65,6 @@ def generateWingBodyMesh(wingTranslation, wingRotation, meshIndex):
 
         }
 
-        # Set guideCurves for the wing case
-        if output_name == 'wing.xyz':
-            options['guideCurves'] = ['w_le_curve']
-
         mesh = pysurf.hypsurf.HypSurfMesh(curve=curve, ref_geom=geom, options=options)
 
         mesh.createMesh()
@@ -86,24 +81,33 @@ def generateWingBodyMesh(wingTranslation, wingRotation, meshIndex):
     # Compute intersection
     Intersections = pysurf.tsurf_tools.compute_intersections([wing,body])
 
-    # Get intersection name
-    intName = Intersections.keys()[0]
-
     # Reorder curve so that it starts at the trailing edge
-    Intersections[intName].shift_end_nodes(criteria='maxX')
+    for intName in Intersections:
+        Intersections[intName].shift_end_nodes(criteria='maxX')
+
+    # Split curves
+    pysurf.tsurf_tools.split_curves(Intersections)
 
     # Remesh curve to get better spacing
-    Intersections[intName] = Intersections[intName].remesh(nNewNodes=300)
+    curveNames = Intersections.keys()
+    Intersections[curveNames[0]] = Intersections[curveNames[0]].remesh(nNewNodes=300)
+    Intersections[curveNames[1]] = Intersections[curveNames[1]].remesh(nNewNodes=5)
 
     # Check if we need to flip the curve
-    if Intersections[intName].coor[2,0] > Intersections[intName].coor[2,-1]:
-        Intersections[intName].flip()
+    if Intersections[curveNames[0]].coor[2,0] > Intersections[curveNames[0]].coor[2,-1]:
+        Intersections[curveNames[0]].flip()
+        Intersections[curveNames[1]].flip()
 
-    Intersections[intName].export_plot3d('curve')
+    # Merge curves
+    mergedCurve = pysurf.tsurf_tools.merge_curves(Intersections,'intersection')
+
+    # Export intersection curve
+    mergedCurve.export_plot3d('curve')
+    print 'Saved intersection curve'
 
     # Add intersection curve to each component
-    wing.add_curve('intersection',Intersections[intName])
-    body.add_curve('intersection',Intersections[intName])
+    wing.add_curve('intersection',mergedCurve)
+    body.add_curve('intersection',mergedCurve)
 
     # GENERATE WING MESH
 
@@ -117,10 +121,8 @@ def generateWingBodyMesh(wingTranslation, wingRotation, meshIndex):
 
         # Set problem
         curve = 'intersection'
-        bc1 = 'curve:te_low_curve'
+        bc1 = 'curve:te_upp_curve'
         bc2 = 'curve:te_upp_curve'
-        # bc1 = 'continuous'
-        # bc2 = 'continuous'
 
         # Set parameters
         epsE0 = 9.5
@@ -176,12 +178,10 @@ def generateWingBodyMesh(wingTranslation, wingRotation, meshIndex):
 
 
     # Run tecplot in bash mode to save picture
-    # os.system('tec360 -b plotMesh.mcr')
-    
-    os.system('tec360 layout_mesh.lay')
+    os.system('tec360 -b plotMesh.mcr')
 
     # Rename image file
-    # os.system('mv images/image.png images/image%03d.png'%(meshIndex))
+    os.system('mv images/image.png images/image%03d.png'%(meshIndex))
 
 
     # Revert transformations to the wing

geometryEngines/TSurf/python/tsurf_component.py
@@ -418,7 +418,7 @@ class TSurfGeometry(Geometry):
         # Return projections
         return xyzProj, tanProj, curveProjDict
 
-    def project_on_curve_d(self, xyz, xyzd, allCoord, xyzProj, curveProjDict):
+    def project_on_curve_d(self, xyz, xyzd, allCoord, xyzProj, tanProj, curveProjDict):
 
         '''
         This function will compute projections and surface Normals
@@ -426,10 +426,12 @@ class TSurfGeometry(Geometry):
         INPUTS:
         xyz -> float[numPts, 3] : Coordinates of the points that should be projected.
 
-        OUTPUTS:
         xyzProj -> float[numPts,3] : Coordinates of the projected points
 
         tanProj -> float[numPts,3] : Curve tangent at projected points
+
+        allCoord -> dictionary : This is a dictionary whose keys are all curve names. Each entry
+                    should contain an array [nNodes x 3] with derivative seeds for all nodes of the curve
 
         Ney Secco 2016-11
         '''
@@ -444,6 +446,7 @@ class TSurfGeometry(Geometry):
 
         # Initialize derivatives
         xyzProjd = np.zeros((numPts,3))
+        tanProjd = np.zeros((numPts,3))
 
         # Call inverse_evaluate for each component in the list, so that we can update
         # dist2, xyzProj, and normProj
@@ -460,12 +463,12 @@ class TSurfGeometry(Geometry):
                 coord = allCoord[curveName]
 
                 # Run projection code in forward mode
-                self.curves[curveName].project_d(xyz, xyzd, coord, xyzProj, xyzProjd, elemIDs, curveMask)
+                self.curves[curveName].project_d(xyz, xyzd, coord, xyzProj, xyzProjd, tanProj, tanProjd, elemIDs, curveMask)
 
         # Return projections derivatives
-        return xyzProjd
+        return xyzProjd, tanProjd
 
-    def project_on_curve_b(self, xyz, xyzProj, xyzProjb, curveProjDict):
+    def project_on_curve_b(self, xyz, xyzProj, xyzProjb, tanProj, tanProjb, curveProjDict):
 
         '''
         This function will backpropagate projections derivatives back
@@ -503,7 +506,7 @@ class TSurfGeometry(Geometry):
 
                 # Run projection code in reverse mode
                 # This will modify xyzb and coorb
-                self.curves[curveName].project_b(xyz, xyzb, coorb, xyzProj, xyzProjb, elemIDs, curveMask)
+                self.curves[curveName].project_b(xyz, xyzb, coorb, xyzProj, xyzProjb, tanProj, tanProjb, elemIDs, curveMask)
 
             # Store curve node derivatives in the corresponding dictionary
             allCoorb[curveName] = coorb
@@ -763,7 +766,7 @@ class TSurfCurve(Curve):
         return curveMask
 
 
-    def project_d(self, xyz, xyzd, coord, xyzProj, xyzProjd, elemIDs, curveMask):
+    def project_d(self, xyz, xyzd, coord, xyzProj, xyzProjd, tanProj, tanProjd, elemIDs, curveMask):
 
         '''
         This function will run forward mode AD to propagate derivatives from inputs (xyz and coor) to outputs (xyzProj).
@@ -805,9 +808,11 @@ class TSurfCurve(Curve):
         curveSearchAPI.curvesearchapi.mindistancecurve_d(xyz.T, xyzd.T,
                                                          self.coor, coord,
                                                          self.barsConn,
-                                                         xyzProj.T, xyzProjd.T, elemIDs, curveMask)
+                                                         xyzProj.T, xyzProjd.T,
+                                                         tanProj.T, tanProjd.T,
+                                                         elemIDs, curveMask)
 
-    def project_b(self, xyz, xyzb, coorb, xyzProj, xyzProjb, elemIDs, curveMask):
+    def project_b(self, xyz, xyzb, coorb, xyzProj, xyzProjb, tanProj, tanProjb, elemIDs, curveMask):
 
         '''
         This function will run forward mode AD to propagate derivatives from inputs (xyz and coor) to outputs (xyzProj).
@@ -849,7 +854,9 @@ class TSurfCurve(Curve):
         curveSearchAPI.curvesearchapi.mindistancecurve_b(xyz.T, xyzb.T,
                                                          self.coor, coorb,
                                                          self.barsConn,
-                                                         xyzProj.T, xyzProjb.T, elemIDs, curveMask)
+                                                         xyzProj.T, xyzProjb.T,
+                                                         tanProj.T, tanProjb.T,
+                                                         elemIDs, curveMask)
 
     def remesh(self, nNewNodes=None, method='linear', spacing='linear',
                initialSpacing=0.1, finalSpacing=0.1):
@@ -886,12 +893,21 @@ class TSurfCurve(Curve):
         '''
 
         # Get connectivities and coordinates of the current Curve object
-        coor = self.coor
-        barsConn = self.barsConn
+        coor = np.array(self.coor,dtype=type(self.coor[0,0]),order='F')
+        barsConn = np.array(self.barsConn,dtype=type(self.barsConn[0,0]),order='F')
 
-        # Get the number of nodes and elements in the curve
+        # Get the number of elements in the curve
         nElem = barsConn.shape[1]
-        nNodes = nElem + 1
+        nNodes = coor.shape[1]
+
+        # Check if the baseline curve is periodic. If this is the case, we artificially repeat
+        # the last point so that we could use the same code of the non-periodic case
+        if barsConn[0,0] == barsConn[1,-1]:
+            periodic = True
+            coor = np.array(np.hstack([coor, coor[:,barsConn[0,0]-1].reshape((3,1))]),dtype=type(self.coor[0,0]),order='F')
+            barsConn[-1,-1] = nNodes+1
+        else:
+            periodic = False
 
         # Use the original number of nodes if the user did not specify any
         if nNewNodes is None:
@@ -1001,23 +1017,18 @@ class TSurfCurve(Curve):
             # Set new nodes
             newCoor = newNodeCoor
 
-            # Check if the baseline curve is periodic
-            if barsConn[0,0] == barsConn[1,-1]:
-                periodic = True
-            else:
-                periodic = False
-
             # Generate new connectivity (the nodes are already in order so we just
             # need to assign an ordered set to barsConn).
             barsConn = np.zeros((2,nNewNodes-1), order='F', dtype=int)
             barsConn[0,:] = range(1,nNewNodes)
             barsConn[1,:] = range(2,nNewNodes+1)
 
-            # We still need to keep periodicity if the original curve is periodic
-            if periodic:
-                barsConn[1,-1] = barsConn[0,0]
-
             newBarsConn = barsConn
+
+        # Adjust connectivities if curve is periodic
+        if periodic:
+            newBarsConn[-1,-1] = newBarsConn[0,0]
+            newCoor = newCoor[:,:-1]
 
         # Create a new curve object and return it
         # This way the original curve coordinates and connectivities remain the same

geometryEngines/TSurf/python/tsurf_tools.py
@@ -516,6 +516,54 @@ def merge_surface_sections(sectionDict, selectedSections):
 
 #=================================================================
 
+def merge_curves(curveDict, mergedCurveName, curvesToMerge=None):
+
+    '''
+    This function will merge curves in the curveDict dictonary whose
+    name is included in curveNames.
+    If curvesToMerge=None, we will merge all curves in curveDict
+
+    Ney Secco 2016-11
+    '''
+
+    # Initialize set of coordinates and connectivities
+    mergedCoor = np.zeros((3,0),order='F')
+    mergedBarsConn = np.zeros((2,0),dtype='int32',order='F')
+
+    # Merge all curves if user provided none
+    if curvesToMerge == None:
+        curvesToMerge = curveDict.keys()
+
+    # Initialize index offset. The new curves cannot reference
+    # nodes of the old ones.
+    indexOffset = 0
+
+    # Loop over every curve
+    for curveName in curveDict:
+        
+        # Check if we want to merge this curve
+        if curveName in curvesToMerge:
+
+            # Get coor and connectivities of this curve.
+            # Remember to apply the offset to the connectivities
+            coor = curveDict[curveName].coor
+            barsConn = curveDict[curveName].barsConn + indexOffset
+
+            # Update offset for nwext curve
+            indexOffset = indexOffset + coor.shape[1]
+
+            # Merge data
+            mergedCoor = np.array(np.hstack([mergedCoor, coor]),order='F')
+            mergedBarsConn = np.array(np.hstack([mergedBarsConn, barsConn]),order='F')
+
+    # Create new curve with the merged data
+    mergedCurve = tsurf_component.TSurfCurve(mergedCoor, mergedBarsConn, mergedCurveName)
+
+    # Return merged curve
+    return mergedCurve
+
+#=================================================================
+
 def split_curves(curveDict, criteria='sharpness'):
 
     '''
@@ -563,6 +611,9 @@ def split_curve_single(curve, curveName, criteria):
     Ney Secco 2016-08
     '''
 
+    # PARAMETERS
+    sharpAngle = 60*np.pi/180.0
+
     # READING INPUTS
 
     # Get coordinates and connectivities
@@ -600,7 +651,7 @@ def split_curve_single(curve, curveName, criteria):
             angle = np.arccos(prevTan.dot(currTan))
 
             # Check if the angle is beyond a certain threshold
-            if angle > 60*np.pi/180.0:
+            if angle > sharpAngle:
 
                 # Store the current element as a break position
                 breakList.append(elemID)
@@ -608,6 +659,30 @@ def split_curve_single(curve, curveName, criteria):
             # Now the current tangent will become the previous tangent of
             # the next iteration
             prevTan = currTan.copy()
+
+        # If the curve is periodic, we need to check for breaks between the first and last elements.
+        if barsConn[0,0] == barsConn[1,-1]:
+
+            # Compute tangent direction of the current element
+            prevTan = coor[:,barsConn[1,-1]-1] - coor[:,barsConn[0,-1]-1]
+            prevTan = prevTan/np.linalg.norm(prevTan)
+
+            # Get the tangent direction of the first bar element
+            currTan = coor[:,barsConn[1,0]-1] - coor[:,barsConn[0,0]-1]
+            currTan = currTan/np.linalg.norm(currTan)
+
+            # Compute change in direction between consecutive tangents
+            angle = np.arccos(prevTan.dot(currTan))
+
+            # Check if the angle is beyond a certain threshold
+            if angle > sharpAngle:
+
+                # We should break the curve at the initial point
+                # so it will not be periodic anymore
+                isPeriodic = False
+
+            else:
+                isPeriodic = True
 
     # CHECK IF BREAKS WERE DETECTED
     # We can stop this function earlier if we detect no break points
@@ -639,7 +714,7 @@ def split_curve_single(curve, curveName, criteria):
 
     # CREATE SPLIT CURVE OBJECTS
 
-    # Now that we have the list of split points (given in splitList), we can create multiple
+    # Now that we have the list of break points (given in breakList), we can create multiple
     # curve objects.
 
     # Count the number of curves that should be between the first and last break point
@@ -675,7 +750,7 @@ def split_curve_single(curve, curveName, criteria):
     # We need to treat periodic curves differently. We use the same check to determine
     # the number of split curves. The periodic case will have minus one curve compared
     # to a non-periodic one.
-    if barsConn[0,0] == barsConn[1,-1]: # Curve is periodic, so we need to define one curve
+    if isPeriodic: # Curve is periodic, so we need to define one curve
 
         # For now copy the original set of nodes
         splitCoor = coor.copy()
@@ -749,13 +824,21 @@ def _remesh_b(origCurve, newCoorb, nNewNodes=None, method='linear', spacing='lin
 
     # Get the number of nodes and elements in the curve
     nElem = barsConn.shape[1]
-    nNodes = nElem + 1
+    nNodes = coor.shape[1]
+
+    # Check if the baseline curve is periodic
+    if barsConn[0,0] == barsConn[1,-1]:
+        periodic = True
+        nNewElems = nNewNodes
+    else:
+        periodic = False
+        nNewElems = nNewNodes-1
 
     # Use the original number of nodes if the user did not specify any
     if nNewNodes is None:
         nNewNodes = nNodes
 
-    _, __, coorb = utilitiesAPI.utilitiesapi.remesh_b(coor, newCoorb, barsConn, method, spacing)
+    _, __, coorb = utilitiesAPI.utilitiesapi.remesh_b(nNewElems, coor, newCoorb, barsConn, method, spacing)
 
     return coorb
 
@@ -769,13 +852,21 @@ def _remesh_d(origCurve, coord, nNewNodes=None, method='linear', spacing='linear
 
     # Get the number of nodes and elements in the curve
     nElem = barsConn.shape[1]
-    nNodes = nElem + 1
+    nNodes = coor.shape[1]
+
+    # Check if the baseline curve is periodic
+    if barsConn[0,0] == barsConn[1,-1]:
+        periodic = True
+        nNewElems = nNewNodes
+    else:
+        periodic = False
+        nNewElems = nNewNodes-1
 
     # Use the original number of nodes if the user did not specify any
     if nNewNodes is None:
         nNewNodes = nNodes
 
-    newCoor, newCoord, newBarsConn = utilitiesAPI.utilitiesapi.remesh_d(nNewNodes, coor, coord, barsConn, method, spacing)
+    newCoor, newCoord, newBarsConn = utilitiesAPI.utilitiesapi.remesh_d(nNewNodes, nNewElems, coor, coord, barsConn, method, spacing)
 
     return newCoord
 
@@ -796,27 +887,34 @@ def compute_intersections(TSurfGeometryList,distTol=1e-7,comm=MPI.COMM_WORLD):
     '''
 
     # Get number of components
-    numGeometrys = len(TSurfGeometryList)
+    numGeometries = len(TSurfGeometryList)
 
     # Stop if user gives only one component
-    if numGeometrys < 2:
+    if numGeometries < 2:
         print 'ERROR: Cannot compute intersections with just one component'
         quit()
 
-    # Initialize list of intersections
-    Intersections = []
+    # Initialize dictionary of intersections
+    Intersections = {}
 
     # Call intersection function for each pair
-    for ii in range(numGeometrys):
-        for jj in range(ii+1,numGeometrys):
+    for ii in range(numGeometries):
+        for jj in range(ii+1,numGeometries):
 
             # Compute new intersections for the current pair
             newIntersections = _compute_pair_intersection(TSurfGeometryList[ii],
-                                                         TSurfGeometryList[jj],
+                                                          TSurfGeometryList[jj],
                                                           distTol, comm)
 
-            # Append new curve objects to the list
-            Intersections = Intersections + newIntersections
+            # Create base name for the new intersection
+            baseName = 'int_'+TSurfGeometryList[ii].name+'_'+TSurfGeometryList[jj].name
+
+            # Append new curve objects to the dictionary
+            intID = 0
+            for curve in newIntersections:
+                intName = baseName + '_%03d'%intID
+                Intersections[intName] = curve
+                intID = intID+1
 
     # Print log
     print 'Computed',len(Intersections),'intersection curves.'

meshTools/hypsurf/python/hypsurf.py
@@ -76,6 +76,12 @@ class HypSurfMesh(object):
         self.numNodes = self.curve.shape[0]
         self.mesh = np.zeros((3, self.numNodes, self.optionsDict['numLayers']))
 
+        # Initialize list to gather dictionary with projection information
+        # that will be used to compute derivatives
+        self.projDict = []
+        self.curveProjDict1 = []
+        self.curveProjDict2 = []
+
         guideIndices = []
         if self.optionsDict['guideCurves']:
             for curve in self.optionsDict['guideCurves']:
@@ -166,9 +172,15 @@ class HypSurfMesh(object):
             # Derivative check
             if deriv_check:
                 rStartd = np.random.random_sample(rStart.shape)
-
                 rStartd_copy = rStartd.copy()
-                R_, Rd, fail, ratios, _ = hypsurfAPI.hypsurfapi.march_d(self.projection, rStart, rStartd, dStart, theta, sigmaSplay, bc1.lower(), bc2.lower(), epsE0, alphaP0, marchParameter, nuArea, ratioGuess, cMax, self.extension_given, numSmoothingPasses, numAreaPasses, numLayers)
+
+                # Set derivatives for underlying surfaces and curves
+                self.coord = np.random.random_sample(self.ref_geom.coor.shape)
+                self.curveCoord = {}
+                for curveName in self.ref_geom.curves:
+                    self.curveCoord[curveName] = np.random.random_sample(self.ref_geom.curves[curveName].coor.shape)
+
+                R_, Rd, fail, ratios, _ = hypsurfAPI.hypsurfapi.march_d(self.projection, self.projection_d, rStart, rStartd, dStart, theta, sigmaSplay, bc1.lower(), bc2.lower(), epsE0, alphaP0, marchParameter, nuArea, ratioGuess, cMax, self.extension_given, numSmoothingPasses, numAreaPasses, numLayers)
 
                 Rb = np.random.random_sample(R.shape)
                 Rb_copy = Rb.copy()
@@ -376,65 +388,83 @@ class HypSurfMesh(object):
         # RETURNS
         return rNext, NNext
 
-    def projection(self, rNext):
+    def projection(self, r):
 
         # Initialize normals array
         NNext = np.zeros((3, self.numNodes))
 
         # Save endpoints
-        node1 = rNext[:3]
-        node2 = rNext[-3:]
+        node1 = r[:3]
+        node2 = r[-3:]
 
         # Project onto surface and compute surface normals
-        rNext, NNext, projDict = self.ref_geom.project_on_surface(rNext.reshape((self.numNodes, 3)))
+        rNext, NNext, projDict = self.ref_geom.project_on_surface(r.reshape((self.numNodes, 3)))
         rNext = rNext.flatten()
         NNext = NNext.T
 
+        # Store projection dictionaries in their corresponding lists
+        self.projDict = self.projDict + [projDict]
+
         # Replace end points if we use curve BCs
         if self.optionsDict['bc1'].lower().startswith('curve'):
-            rNext[:3], NNextAux, curveProjDict = self.ref_geom.project_on_curve(node1.reshape((1, 3)), curveCandidates=[self.ref_curve1])
+            rNext[:3], NNextAux, curveProjDict1 = self.ref_geom.project_on_curve(node1.reshape((1, 3)), curveCandidates=[self.ref_curve1])
             NNext[:, 0] = NNextAux.T[:, 0]
+            self.curveProjDict1 = self.curveProjDict1 + [curveProjDict1]
 
         if self.optionsDict['bc2'].lower().startswith('curve'):
-            rNext[-3:], NNextAux, curveProjDict = self.ref_geom.project_on_curve(node2.reshape((1, 3)), curveCandidates=[self.ref_curve2])
+            rNext[-3:], NNextAux, curveProjDict2 = self.ref_geom.project_on_curve(node2.reshape((1, 3)), curveCandidates=[self.ref_curve2])
             NNext[:, -1] = NNextAux.T[:, 0]
+            self.curveProjDict2 = self.curveProjDict2 + [curveProjDict2]
 
         if self.guideIndices:
             for i, index in enumerate(self.guideIndices):
                 curve = self.optionsDict['guideCurves'][i]
                 node = rNext[3*index:3*index+3]
-                rNext[3*index:3*index+3], NNextAux, curveProjDict = self.ref_geom.project_on_curve(node.reshape((1, 3)), curveCandidates=[curve])
+                rNext[3*index:3*index+3], NNextAux, curveProjDict = self.ref_geom.project_on_curve(node.reshape((1, 3)),
+                                                                                                   curveCandidates=[curve])
                 NNext[:, index] = NNextAux.T[:, 0]
-
 
         return rNext, NNext
 
-    '''
-    def projection_d(self, rNext, rNext):
-
-        # Initialize normals array
-        NNext = np.zeros((3, self.numNodes))
+    def projection_d(self, r, rd, rNext, NNext):
 
         # Save endpoints
-        node1 = rNext[:3]
-        node2 = rNext[-3:]
+        node1 = r[:3].reshape((1, 3))
+        node2 = r[-3:].reshape((1, 3))
+        node1d = rd[:3].reshape((1, 3))
+        node2d = rd[-3:].reshape((1, 3))
+
+        # Pop the first projection dictionary from the list (since we are propagating derivatives forward)
+        projDict = self.projDict.pop(0)
 
         # Project onto surface and compute surface normals
-        rNext, NNext, projDict = self.ref_geom.project_on_surface(rNext.reshape((self.numNodes, 3)))
-        rNext = rNext.flatten()
-        NNext = NNext.T
+        rNextd, NNextd = self.ref_geom.project_on_surface_d(r.reshape((self.numNodes, 3)),
+                                                            rd.reshape((self.numNodes, 3)),
+                                                            rNext.reshape((self.numNodes, 3)),
+                                                            NNext.T,
+                                                            projDict,
+                                                            self.coord)
+        rNextd = rNextd.flatten()
+        NNextd = NNextd.T
 
         # Replace end points if we use curve BCs
         if self.optionsDict['bc1'].lower().startswith('curve'):
-            rNext[:3], NNextAux = self.ref_geom.project_on_curve(node1.reshape((1, 3)), curveCandidates=[self.ref_curve1])
-            NNext[:, 0] = NNextAux.T[:, 0]
+            curveProjDict1 = self.curveProjDict1.pop(0)
+            rNextd[:3], NNextAuxd = self.ref_geom.project_on_curve_d(node1, node1d,
+                                                                     self.curveCoord,
+                                                                     rNext[:3], NNext[:,0],
+                                                                     curveProjDict1)
+            NNextd[:, 0] = NNextAuxd.T[:, 0]
 
         if self.optionsDict['bc2'].lower().startswith('curve'):
-            rNext[-3:], NNextAux = self.ref_geom.project_on_curve(node2.reshape((1, 3)), curveCandidates=[self.ref_curve2])
-            NNext[:, -1] = NNextAux.T[:, 0]
+            curveProjDict2 = self.curveProjDict2.pop(0)
+            rNextd[-3:], NNextAuxd = self.ref_geom.project_on_curve_d(node2, node2d,
+                                                                      self.curveCoord,
+                                                                      rNext[-3:], NNext[:,-1],
+                                                                      curveProjDict2)
+            NNextd[:, -1] = NNextAuxd.T[:, -1]
 
-        return rNext, NNext
-    '''
+        return rNextd, NNextd
 
     def areaFactor(self, r0, d):
 
@@ -757,13 +787,37 @@ class HypSurfMesh(object):
             # Set guideCurve matrix contributions
             if self.guideIndices:
                 if index in self.guideIndices:
+
+
                     # Populate matrix
                     K[3*index:3*index+3,3*index:3*index+3] = np.eye(3)/2
                     K[3*index:3*index+3,3*index-3:3*index-3+3] = np.eye(3)/4
                     K[3*index:3*index+3,3*index+3:3*index+3+3] = np.eye(3)/4
 
                     f[3*index:3*index+3] = S0[index] * N0[:,index]
+                    '''
 
+                    # Call assembly routine
+                    matrixBuilder(index)
+
+                    # Get dissipation factor from original matrix
+                    epsI_local = (K[3*index,3*index] - 1.0)/2.0
+                    epsE_local = epsI_local/2.0
+
+                    # Here we keep the same diagonal matrix, but we modify
+                    # the neighbors' matrices to keep only dissipation terms
+                    K[3*index:3*index+3,3*index-3:3*index-3+3] = -epsI_local*np.eye(3)
+                    K[3*index:3*index+3,3*index+3:3*index+3+3] = -epsI_local*np.eye(3)
+
+                    # Compute dissipation term for RHS
+                    r_prev = r0[3*(index-1):3*(index-1)+3]
+                    r_curr = r0[3*(index):3*(index)+3]
+                    r_next = r0[3*(index+1):3*(index+1)+3]
+                    De = epsE_local*(r_prev - 2*r_curr + r_next)
+
+                    # Replace RHS to keep marching direction and dissipation term
+                    f[3*index:3*index+3] = S0[index] * N0[:,index] + De
+                    '''
                 else:
                     # Call assembly routine
                     matrixBuilder(index)
@@ -1252,6 +1306,7 @@ def plotGrid(X,Y,Z,show=False):
     ax.set_axis_off()
 
     if show:
+        # Show
         plt.show()
 
     # Return the figure handle