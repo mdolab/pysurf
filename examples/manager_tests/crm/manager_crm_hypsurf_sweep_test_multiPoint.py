Fixed bug in curve projection derivatives. Don't use same variable twice when calling fortran subroutines differentiated by Tapenade. Added CRM example for hypsurf tests.

examples/manager_tests/crm/manager_crm_hypsurf_sweep_test_multiPoint.py
@@ -9,22 +9,34 @@ import pickle
 
 # MESH PARAMETERS
 numSkinNodes = 129
-LE_spacing = 0.01
-TE_spacing = 0.001
+LE_spacing = 0.001
+TE_spacing = 0.01
+
+# WING POSITIONS
+deltaZ = np.linspace(0.000001, 3.0, 11)
+
+# TRACKING POINT
+# Give i coordinate that we will use to create the tracking slice
+i_node = 190
+j_list = [0, 10, 20, 30, 40, 48]
 
 # TESTING FUNCTION
 
 os.system('rm *.plt')
+os.system('rm *.xyz')
 
 # Load components
 comp1 = pysurf.TSurfGeometry('../../inputs/initial_full_wing_crm4.cgns',['wing','curve_le'])
 comp2 = pysurf.TSurfGeometry('../../inputs/fuselage_crm4.cgns',['fuse'])
 
-name1 = comp1.name
-name2 = comp2.name
+#name1 = 'wing'
+#name2 = 'body'
 
 #comp1.rename(name1)
 #comp2.rename(name2)
+
+name1 = comp1.name
+name2 = comp2.name
 
 # Load TE curves and append them to the wing component
 curve_te_upp = pysurf.tsurf_tools.read_tecplot_curves('curve_te_upp.plt_')
@@ -35,24 +47,27 @@ curveName = curve_te_low.keys()[0]
 comp1.add_curve(curve_te_low[curveName])
 comp1.curves[curveName].flip()
 
-# Translate the wing
-comp1.translate(0.0, 0.0, 0.0001)
-
 # Create manager object and add the geometry objects to it
-manager0 = pysurf.Manager()
-manager0.add_geometry(comp1)
-manager0.add_geometry(comp2)
+manager = pysurf.Manager()
+manager.add_geometry(comp1)
+manager.add_geometry(comp2)
 
 distTol = 1e-7
 
 #======================================================
-# FORWARD PASS
 
-def forward_pass(manager):
+def compute_position(wing_deltaZ, i_node, j_node):
 
     '''
-    This function will apply all geometry operations to the given manager.
+    This function will apply all geometry operations to compute the
+    intersection and its derivative at the new location.
     '''
+
+    # Clear the manager object first
+    manager.clear_all()
+
+    # Translate the wing
+    manager.geoms[name1].translate(0.0, 0.0, wing_deltaZ)
 
     # INTERSECT
 
@@ -98,6 +113,7 @@ def forward_pass(manager):
             # We have an upper or lower skin curve.
             # First let's identify if the curve is defined from
             # LE to TE or vice-versa
+
             curveCoor = curve.get_points()
             deltaX = curveCoor[0,-1] - curveCoor[0,0]
 
@@ -118,8 +134,8 @@ def forward_pass(manager):
 
                     optionsDict = {'nNewNodes':numSkinNodes,
                                    'spacing':'hypTan',
-                                   'initialSpacing':TE_spacing,
-                                   'finalSpacing':LE_spacing}
+                                   'initialSpacing':LE_spacing,
+                                   'finalSpacing':TE_spacing}
 
                     LS_curveName = manager.remesh_intCurve(curveName, optionsDict)
 
@@ -127,8 +143,8 @@ def forward_pass(manager):
 
                     optionsDict = {'nNewNodes':numSkinNodes,
                                    'spacing':'hypTan',
-                                   'initialSpacing':LE_spacing,
-                                   'finalSpacing':TE_spacing}
+                                   'initialSpacing':TE_spacing,
+                                   'finalSpacing':LE_spacing}
 
                     LS_curveName = manager.remesh_intCurve(curveName, optionsDict)
 
@@ -140,8 +156,8 @@ def forward_pass(manager):
 
                     optionsDict = {'nNewNodes':numSkinNodes,
                                    'spacing':'hypTan',
-                                   'initialSpacing':TE_spacing,
-                                   'finalSpacing':LE_spacing}
+                                   'initialSpacing':LE_spacing,
+                                   'finalSpacing':TE_spacing}
 
                     US_curveName = manager.remesh_intCurve(curveName, optionsDict)
 
@@ -149,28 +165,28 @@ def forward_pass(manager):
 
                     optionsDict = {'nNewNodes':numSkinNodes,
                                    'spacing':'hypTan',
-                                   'initialSpacing':LE_spacing,
-                                   'finalSpacing':TE_spacing}
+                                   'initialSpacing':TE_spacing,
+                                   'finalSpacing':LE_spacing}
 
                     US_curveName = manager.remesh_intCurve(curveName, optionsDict)
 
-    # MERGE
+    # Now we can merge the new curves
     curveNames = [TE_curveName, LS_curveName, US_curveName]
     mergedCurveName = 'intersection'
     manager.merge_intCurves(curveNames, mergedCurveName)
 
     # REORDER
     manager.intCurves[mergedCurveName].shift_end_nodes(criteria='maxX')
-
-    # Export final curve
-    manager.intCurves[mergedCurveName].export_tecplot(mergedCurveName)
-
+    
     # Flip the curve for marching if necessary
     mergedCurveCoor = manager.intCurves[mergedCurveName].get_points()
     deltaZ = mergedCurveCoor[2,1] - mergedCurveCoor[2,0]
 
     if deltaZ > 0:
         manager.intCurves[mergedCurveName].flip()
+
+    # Export final curve
+    manager.intCurves[mergedCurveName].export_tecplot(mergedCurveName)
 
     # MARCH SURFACE MESHES
     meshName = 'mesh'
@@ -220,153 +236,74 @@ def forward_pass(manager):
     for meshName in meshNames:
         manager.meshes[meshName].exportPlot3d(meshName+'.xyz')
 
-    return mergedCurveName
+    # DERIVATIVE SEEDS
 
-# END OF forward_pass
+    # Get spanwise position of the tracked node
+    Y = manager.meshes[meshNames[0]].mesh[1, i_node, j_node]
+
+    # Set up derivative seeds so we can compute the derivative of the spanwise position
+    # of the tracked node of the intersection
+    meshb = np.zeros(manager.meshes[meshNames[0]].mesh.shape)
+    meshb[1, i_node, j_node] = 1.0
+
+    # Set derivatives to the mesh
+    manager.meshes[meshNames[0]].set_reverseADSeeds(meshb)
+
+    # REVERSE AD
+    
+    # Call AD code
+    manager.reverseAD()
+    
+    # Get relevant seeds
+    coor1b = manager.geoms[name1].get_reverseADSeeds()
+    coor2b = manager.geoms[name2].get_reverseADSeeds()
+    curveCoorb = []
+    for curveName in manager.geoms[name1].curves:
+        curveCoorb.append(manager.geoms[name1].curves[curveName].get_reverseADSeeds())
+
+    # Condense derivatives to take into acount the translation
+    dYdZ = 0.0
+    dYdZ = np.sum(coor1b[2,:])
+    for ii in range(len(curveCoorb)):
+        dYdZ = dYdZ + np.sum(curveCoorb[ii][2,:])
+
+    # Move the wing back to its original position
+    manager.geoms[name1].translate(0.0, 0.0, -wing_deltaZ)
+
+    # Return the spanwise position of the highest intersection node and its derivative
+    return Y, dYdZ
+
+# END OF compute_position
 #======================================================
 
-# Call the forward pass function to the original manager
-mergedCurveName = forward_pass(manager0)
+# Loop for every j position
+for j_node in j_list:
 
-# DERIVATIVE SEEDS
-
-# Generate random seeds
-manager0.geoms[name1].set_randomADSeeds(mode='forward')
-
-for curveName in comp1.curves:
-    manager0.geoms[name1].curves[curveName].set_randomADSeeds(mode='forward')
-
-manager0.geoms[name2].set_randomADSeeds(mode='forward')
-
-for curveName in comp2.curves:
-    manager0.geoms[name2].curves[curveName].set_randomADSeeds(mode='forward')
-
-for mesh in manager0.meshes.itervalues():
-    mesh.set_randomADSeeds(mode='reverse')
-
-# Store relevant seeds
-coor1d = manager0.geoms[name1].get_forwardADSeeds()
-
-curveCoor1d = []
-for curveName in comp1.curves:
-    curveCoor1d.append(manager0.geoms[name1].curves[curveName].get_forwardADSeeds())
-
-coor2d = manager0.geoms[name2].get_forwardADSeeds()
-
-curveCoor2d = []
-for curveName in comp2.curves:
-    curveCoor2d.append(manager0.geoms[name2].curves[curveName].get_forwardADSeeds())
-
-meshb = []
-for mesh in manager0.meshes.itervalues():
-    meshb.append(mesh.get_reverseADSeeds(clean=False))
-
-# FORWARD AD
-
-# Call AD code
-manager0.forwardAD()
-
-# Get relevant seeds
-meshd = []
-for mesh in manager0.meshes.itervalues():
-    meshd.append(mesh.get_forwardADSeeds())
-
-# REVERSE AD
-
-# Call AD code
-manager0.reverseAD()
+    # Now we will execute the function for every wing position
     
-# Get relevant seeds
-coor1b = manager0.geoms[name1].get_reverseADSeeds()
+    # Initialize arrays to hold results
+    numPos = len(deltaZ)
+    Y = np.zeros(numPos)
+    dYdZ = np.zeros(numPos)
 
-curveCoor1b = []
-for curveName in comp1.curves:
-    curveCoor1b.append(manager0.geoms[name1].curves[curveName].get_reverseADSeeds())
+    # Compute intersection for every wing position
+    for ii in range(numPos):
+        print ''
+        print 'translation'
+        print deltaZ[ii]
+        print 'i_node'
+        print i_node
+        print 'j_node'
+        print j_node
+        print ''
+        Y[ii], dYdZ[ii] = compute_position(deltaZ[ii], i_node, j_node)
+        print ''
+        print 'results'
+        print Y[ii]
+        print dYdZ[ii]
+        print ''
 
-coor2b = manager0.geoms[name2].get_reverseADSeeds()
+    results = np.vstack([deltaZ,Y,dYdZ])
+    with open('results_%03d_%03d.pickle'%(i_node,j_node),'w') as fid:
+        pickle.dump(results,fid)
 
-curveCoor2b = []
-for curveName in comp2.curves:
-    curveCoor2b.append(manager0.geoms[name2].curves[curveName].get_reverseADSeeds())
-
-# Dot product test
-dotProd = 0.0
-
-for ii in range(len(meshd)):
-    dotProd = dotProd + np.sum(meshd[ii]*meshb[ii])
-
-dotProd = dotProd - np.sum(coor1b*coor1d)
-
-for ii in range(len(curveCoor1d)):
-    dotProd = dotProd - np.sum(curveCoor1d[ii]*curveCoor1b[ii])
-
-dotProd = dotProd - np.sum(coor2b*coor2d)
-
-for ii in range(len(curveCoor2d)):
-    dotProd = dotProd - np.sum(curveCoor2d[ii]*curveCoor2b[ii])
-
-print 'dotProd test (this will be repeated at the end as well)'
-print dotProd
-
-# FINITE DIFFERENCE
-stepSize = 1e-7
-
-# Apply perturbations to the geometries
-comp1.update(comp1.coor + stepSize*coor1d)
-
-ii = 0
-for curveName in comp1.curves:
-    comp1.curves[curveName].set_points(comp1.curves[curveName].get_points() + stepSize*curveCoor1d[ii])
-    ii = ii + 1
-
-comp2.update(comp2.coor + stepSize*coor2d)
-
-ii = 0
-for curveName in comp2.curves:
-    comp2.curves[curveName].set_points(comp2.curves[curveName].get_points() + stepSize*curveCoor2d[ii])
-    ii = ii + 1
-
-# Create new manager with perturbed components
-manager1 = pysurf.Manager()
-manager1.add_geometry(comp1)
-manager1.add_geometry(comp2)
-
-# Do the forward pass to the perturbed case
-mergedCurveName = forward_pass(manager1)
-
-# Get coordinates of the meshes in both cases to compute FD derivatives
-meshd_FD = []
-for meshName in manager0.meshes:
-    meshCoor0 = manager0.meshes[meshName].mesh[:,:,:]
-    meshCoor1 = manager1.meshes[meshName].mesh[:,:,:]
-
-    # Compute derivatives with FD
-    meshCoord_FD = (meshCoor1 - meshCoor0)/stepSize
-
-    # Append to the dictionary
-    meshd_FD.append(meshCoord_FD)
-
-def view_mat(mat):
-    """ Helper function used to visually examine matrices. """
-    import matplotlib.pyplot as plt
-    if len(mat.shape) > 2:
-        mat = np.sum(mat, axis=2)
-    # print "Cond #:", np.linalg.cond(mat)
-    im = plt.imshow(mat, interpolation='none')
-    plt.colorbar(im, orientation='horizontal')
-    plt.show()
-
-# Find the largest difference in derivatives
-FD_error = 0.0
-for ii in range(len(manager0.meshes)):
-    curr_error = np.max(np.abs(meshd[ii] - meshd_FD[ii]))
-    view_mat(np.abs(meshd[ii][0,:,:] - meshd_FD[ii][0,:,:]))
-    view_mat(np.abs(meshd[ii][1,:,:] - meshd_FD[ii][1,:,:]))
-    view_mat(np.abs(meshd[ii][2,:,:] - meshd_FD[ii][2,:,:]))
-    FD_error = max(FD_error, curr_error)
-
-# Print results
-print 'dotProd test'
-print dotProd
-print 'FD test'
-print FD_error

geometryEngines/TSurf/python/tsurf_component.py
@@ -894,7 +894,7 @@ class TSurfCurve(Curve):
 
         # Assing coor and barsConn. Remember to crop coor to get unique nodes only
         self.coor = np.array(coor[:,:nUniqueNodes], order='F')
-        self.barsConn = sortedConn
+        self.barsConn = np.array(sortedConn, dtype='int32', order='F')
         self.name = name
         self.numNodes = self.coor.shape[1]
 
@@ -921,11 +921,16 @@ class TSurfCurve(Curve):
 
     def flip(self):
 
+        '''
         # Flip elements
         self.barsConn = self.barsConn[::-1]
         # Flip nodes
         for ii in range(len(self.barsConn)):
             self.barsConn[ii] = self.barsConn[ii][::-1]
+        '''
+
+        self.barsConn = np.array(self.barsConn[::-1,::-1],order='F')
+
         # Flip the extra data
 
         for data in self.extra_data:
@@ -942,7 +947,7 @@ class TSurfCurve(Curve):
         tst.rotate(self, angle, axis)
 
     def update(self, coor):
-        self.coor = coor
+        self.coor = np.array(coor,order='F')
 
     def project(self, xyz, dist2=None, xyzProj=None, tangents=None, elemIDs=None):
 

meshTools/hypsurf/python/hypsurf.py
@@ -20,7 +20,6 @@ TO DO
 - blend the angle-based dissipation coefficient
 '''
 
-
 class HypSurfMesh(object):
     """
     Hyperbolic surface mesh created by projecting points extruded from a
@@ -663,11 +662,19 @@ class HypSurfMesh(object):
                 node = r[3*index:3*index+3].reshape((1, 3))
                 curve = self.optionsDict['guideCurves'][i]
 
+
                 rbAux = self.ref_geom.project_on_curve_b(node,
                                                          rNext[3*index:3*index+3], rNextb[3*index:3*index+3],
                                                          NNext[:,index], NNextb[:,index],
                                                          curveProjDict)
-
+                '''
+                elemIDs = curveProjDict['elemIDs']
+                curveMask = [1]
+                nodeb = np.zeros(node.shape)
+                self.ref_geom.curves[curve].project_b(node, nodeb, rNext[3*index:3*index+3], rNextb[3*index:3*index+3],
+                                                      NNext[:,index], NNextb[:,index], elemIDs, curveMask)
+                rbAux = nodeb
+                '''
                 rb[3*index:3*index+3] = rbAux
 
         return rb
@@ -960,7 +967,7 @@ class HypSurfMesh(object):
         print 'Dot product test for Projection (this should be around 1e-14):'
         print dotprod
         print ''
-        
+
         # FORWARD DERIVATIVES WITH FINITE DIFFERENCING
 
         stepSize = 1e-7