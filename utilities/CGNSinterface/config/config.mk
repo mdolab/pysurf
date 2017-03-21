a935ef0a02a66f040c77f6988234fc4d9e5ff096
Ney Secco <ney.rafa@gmail.com>
2017-03-21 15:25:11 -0500

    Added intel compiler config files

--- utilities/CGNSinterface/config/config.mk
+++ utilities/CGNSinterface/config/config.mk
@@ -14,10 +14,10 @@ CGNS_INCLUDE_FLAGS=-I$(HOME)/packages/cgnslib_3.2.1/src
 CGNS_LINKER_FLAGS=-L$(HOME)/packages/cgnslib_3.2.1/src -lcgns
 
 # ------- Define Compiler Flags ----------------------------------------
-FF90_GEN_FLAGS = -fPIC -g -fbounds-check
+FF90_GEN_FLAGS = -fPIC -g
 CC_GEN_FLAGS   = -fPIC
 
-FF90_OPT_FLAGS   =  -fPIC -fdefault-real-8 -O2 -fdefault-double-8
+FF90_OPT_FLAGS   =  -fPIC -r8 -O2
 CC_OPT_FLAGS     = -O2
 
 # ------- Define Archiver  and Flags -----------------------------------
