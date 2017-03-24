f1618f277478545793991009b24d378c46693190
Ney Secco <neysecco@umich.edu>
2017-03-24 15:59:40 -0400

    Changed intersection api to avoid Python access to allocatable variables.

--- geometryEngines/TSurf/src/CGNSInterface/getf2pySignature.sh
+++ geometryEngines/TSurf/src/CGNSInterface/getf2pySignature.sh
@@ -1,2 +1,8 @@
+# Use f2py to generate preliminary signature file
 f2py cgnsAPI.F90 -m cgnsAPI -h cgnsAPI.pyf --overwrite-signature
+
+# Remove allocatable variables from the signature
+sed -i '/allocatable/d' cgnsAPI.pyf
+
+# Move signature file to f2py folder
 mv cgnsAPI.pyf ../python/f2py/cgnsAPI.pyf
