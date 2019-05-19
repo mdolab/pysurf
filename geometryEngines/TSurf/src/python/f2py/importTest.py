570245ed7915a70ee0a756d392d4270b0255c10d
Anil Yildirim <anily@umich.edu>
2019-05-19 12:31:20 -0400

    Fixing compilation related issues with python3.

--- geometryEngines/TSurf/src/python/f2py/importTest.py
+++ geometryEngines/TSurf/src/python/f2py/importTest.py
@@ -9,16 +9,16 @@ modules = ['cgnsAPI',
            'utilitiesAPI']
 
 for name in modules:
-    print ''
-    print "Testing if module %s can be imported..." % name
+    print ('')
+    print ("Testing if module %s can be imported..." % name)
     import_cmd = "import %s" % name
     try:
-        exec import_cmd
-    except Exception, inst:
-        print "Error: %s." % inst
+        exec (import_cmd)
+    except (Exception, inst):
+        print ("Error: %s." % inst)
         sys.exit(1)
     # end try
 
-    print "OK! Module %s was successfully imported." % name
+    print ("OK! Module %s was successfully imported." % name)
 
-print ''
+print ('')
--- meshTools/hypsurf/src/python/f2py/importTest.py
+++ meshTools/hypsurf/src/python/f2py/importTest.py
@@ -5,16 +5,16 @@ import sys
 modules = ['hypsurfAPI']
 
 for name in modules:
-    print ''
-    print "Testing if module %s can be imported..." % name
+    print ('')
+    print ("Testing if module %s can be imported..." % name)
     import_cmd = "import %s" % name
     try:
-        exec import_cmd
-    except Exception, inst:
-        print "Error: %s." % inst
+        exec (import_cmd)
+    except (Exception, inst):
+        print ("Error: %s." % inst)
         sys.exit(1)
     # end try
 
-    print "OK! Module %s was successfully imported." % name
-    
-print ''
+    print ("OK! Module %s was successfully imported." % name)
+
+print ('')
