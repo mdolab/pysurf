1b381939d481d551b4ef9e00539a91612f04b5d4
Eirikur Jonsson <eirikurj@umich.edu>
2022-10-06 17:22:56 +0000

    Update library import checks

--- src/f2py/importTest.py
+++ src/f2py/importTest.py
@@ -1,16 +1,31 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+# Standard Python modules
+import argparse
+import os
 import sys
 
-modules = ["cgnsAPI", "adtAPI", "curveSearchAPI", "intersectionAPI", "utilitiesAPI"]
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "name",
+    nargs="+",
+    help="One or more library names (example: libpackage.so). Note: This script must be run in the same dir as the library.",
+)
+args = parser.parse_args()
 
-for name in modules:
-    print("")
-    print(f"Testing if module {name} can be imported...")
-    import_cmd = f"import {name}"
+# Make sure the executing directory is always in the PATH before importing
+sys.path.insert(0, os.getcwd())
+
+for libraryName in args.name:
+    # Only get the filename without the extension
+    moduleName = os.path.splitext(libraryName)[0]
+    print(f"Testing if module {moduleName} can be imported...")
+
     try:
+        import_cmd = f"import {moduleName}"
         exec(import_cmd)
-    except ImportError:
-        print(f"Error importing {name}")
+    except ImportError as e:
+        print(f"Error: {e}")
+        print(f"Error: library {libraryName} was not imported correctly")
         sys.exit(1)
 
-    print(f"Module {name} was successfully imported.")
+    print(f"Module {moduleName} was successfully imported")
