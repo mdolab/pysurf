fffb8afdebd656de45a8c8e4c39b7ddcc08ea89a
sseraj <sseraj@umich.edu>
2023-08-02 14:17:27 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open("doc/requirements.txt") as f:
+    docs_require = f.read().splitlines()
+
 setup(
     name="pysurf",
     version=__version__,
@@ -30,7 +33,11 @@ setup(
         "numpy>=1.16",
         "mpi4py>=3.0",
         "scipy",
+        "complexify",
     ],
-    extras_require={"testing": ["testflo"]},
+    extras_require={
+        "docs": docs_require,
+        "testing": ["testflo"],
+    },
     classifiers=["Operating System :: Linux", "Programming Language :: Python, Fortran"],
 )
