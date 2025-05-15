8cfe47b97b7ab31edd450b110074e9f7bb032d34
Eirikur Jonsson <36180221+eirikurj@users.noreply.github.com>
2025-05-15 15:50:44 +0000

    Update link for input files (#39)

--- input_files/get-input-files.sh
+++ input_files/get-input-files.sh
@@ -3,6 +3,6 @@
 
 DIR=$(dirname $0)
 TAR="input_files.tar.gz"
-wget -O $DIR/$TAR http://umich.edu/~mdolaboratory/repo_files/pySurf/pysurf_input_files.tar.gz
+wget -O $DIR/$TAR https://websites.umich.edu/~mdolaboratory/repo_files/pySurf/pysurf_input_files.tar.gz
 tar -xzf $DIR/$TAR -C $DIR/../
 rm $DIR/$TAR
